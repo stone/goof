5d639ed487f550016e298a9b92b059e79c521937
Fredrik Steen <fredrik.steen@redpill-linpro.com>
2012-01-17 14:01:37 +0100

    Changelog, upload, renaming, fix

--- goof.go
+++ goof.go
@@ -1,25 +1,42 @@
 package main
 
+/* goof - a woof like server written in go
+A very simple http server that serves files from
+the current directory with the possibility to
+receive files via upload form. 
+
+Originally based on SimpleHttpd by mpl (github)
+Written by Fredrik Steen <stone@ppo2.se>
+
+Changelog:
+
+v0.1:
+  * Added possibility to make goof a download only server.
+  * Added some sensible logging.
+  * Added a starting banner.
+  * Added error handler when starting up.
+*/
+
 import (
 	"bytes"
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"path"
 )
 
-const uploadform = "upload.html"
-
 var (
 	host       = flag.String("host", "0.0.0.0:8080", "listening port and hostname")
+	noUpload   = flag.Bool("n", false, "only allow downloads")
 	help       = flag.Bool("h", false, "show this help")
 	rootdir, _ = os.Getwd()
 )
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "\t httpd \n")
+	fmt.Fprintln(os.Stderr, "usage: goof [flags]")
 	flag.PrintDefaults()
 	os.Exit(2)
 }
@@ -39,13 +56,25 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 
 // because getting a 404 when trying to use http.FileServer. beats me.
 func myFileServer(w http.ResponseWriter, r *http.Request, url string) {
+	log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL.RawPath)
 	http.ServeFile(w, r, path.Join(rootdir, url))
 }
 
-func uploadHandler(rw http.ResponseWriter, req *http.Request, url string) {
-	mr, err := req.MultipartReader()
+// A restricted version that only serves specified file
+//func myFileServer(w http.ResponseWriter, r *http.Request, url string, filename string) {
+//	fmt.Println("url:", url)
+//	fmt.Println("filename:", filename)
+//	http.ServeFile(w, r, path.Join(rootdir, url))
+//
+//}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request, url string) {
+	mr, err := r.MultipartReader()
+
 	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		// We did not receive any file so we serve up the form instead
+		// or maybe we should redirect to a form handler?
+		uploadFormHandler(w, r, url)
 		return
 	}
 
@@ -54,42 +83,51 @@ func uploadHandler(rw http.ResponseWriter, req *http.Request, url string) {
 		if err == io.EOF {
 			break
 		}
+
 		if err != nil {
-			http.Error(rw, "reading body: "+err.Error(), http.StatusInternalServerError)
+			http.Error(w, "reading body: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
+
 		fileName := part.FileName()
 		if fileName == "" {
 			continue
 		}
-		println("fileName: " + fileName)
+
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, fileName)
+
 		buf := bytes.NewBuffer(make([]byte, 0))
 		_, err = io.Copy(buf, part)
 		if err != nil {
-			http.Error(rw, "copying: "+err.Error(), http.StatusInternalServerError)
+			http.Error(w, "copying: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
+
 		f, err := os.Create(path.Join(rootdir, fileName))
 		if err != nil {
-			http.Error(rw, "opening file: "+err.Error(), http.StatusInternalServerError)
+			http.Error(w, "opening file: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 		defer f.Close()
+
 		_, err = buf.WriteTo(f)
 		if err != nil {
-			http.Error(rw, "writing: "+err.Error(), http.StatusInternalServerError)
+			http.Error(w, "writing: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
+
 		break
 	}
-	http.ServeFile(rw, req, path.Join(rootdir, uploadform))
+	http.Redirect(w, r, "/", 303)
 }
 
-func createUploadForm() {
+// Called from myFileServer to serve the upload form
+func uploadFormHandler(w http.ResponseWriter, r *http.Request, url string) {
+	//TODO: make below a constant
 	contents := `
 <html>
 <head>
-  <titleUpload file</title>
+  <title>Upload file</title>
 </head>
 <body>
   <h1>Upload file</h1>
@@ -102,17 +140,8 @@ func createUploadForm() {
 </body>
 </html>
 `
-	f, err := os.Create(path.Join(rootdir, uploadform))
-	if err != nil {
-		println("err creating uploadform")
-		os.Exit(2)
-	}
-	defer f.Close()
-	_, err = f.Write([]byte(contents))
-	if err != nil {
-		println("err writing uploadform")
-		os.Exit(2)
-	}
+	log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL.RawPath)
+	fmt.Fprintf(w, contents)
 }
 
 func main() {
@@ -127,9 +156,16 @@ func main() {
 		usage()
 	}
 
-	createUploadForm()
-
-	http.HandleFunc("/upload", makeHandler(uploadHandler))
+	// if flag is set we don't register the uploadHandler
+	if *noUpload == false {
+		http.HandleFunc("/upload", makeHandler(uploadHandler))
+	}
 	http.Handle("/", makeHandler(myFileServer))
-	http.ListenAndServe(*host, nil)
+
+	log.Printf("Serving %s on http://%s/", rootdir, *host)
+
+	err := http.ListenAndServe(*host, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
